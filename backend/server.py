1fc32488bf4876f96e0594389b7a0f4b762a2e69
SamEThibault <sam5thibault@gmail.com>
2024-02-10 14:21:38 -0500

    HE HELL NAH

--- backend/server.py
+++ backend/server.py
@@ -3,9 +3,7 @@ import random
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from dotenv import load_dotenv
-from Instructions import prompt_response
-from RelatedQuestions import RelatedQuestions
-from concurrent.futures import ThreadPoolExecutor
+from roleplay import main
 
 load_dotenv()
 app = Flask(__name__, static_url_path="", static_folder='./build', template_folder='build')
@@ -19,60 +17,19 @@ app.config['SECRET_KEY'] =  os.getenv("secret_key")
 def frontend():
     return app.send_static_file('index.html')
 
-# To call OpenAI API
+@app.route("/init", methods=["GET"])
+def get_init():
+    return {"response": main()}
+
+# Generic route to prompt GPT and return response
 # Requires: json data: {"prompt" : "A chatGPT prompt"}
 @app.route("/response", methods=["POST"])
 @cross_origin()
 def get_response():
-    # Use prompt to call OpenAI API
     prompt = request.json['prompt']
-    res = prompt_response("Write a long detailed list answer to the question:" + prompt)
+    res = None
+    # Call whatever function to prompt GPT
     return {"response" : res}
-
-
-
-# To get suggestions and their contents
-@app.route("/suggestions", methods=["POST"])
-@cross_origin()
-def get_suggestions():
-    # Call Google suggestion API
-    prompt = request.json['prompt']
-    suggest = RelatedQuestions()
-    prompt = request.json['prompt']
-    related_questions = suggest.get_related_questions(prompt)
-    related_searches = suggest.get_related_searches(prompt)
-    
-    # Add stuff to list depending on if we got results from the API or not
-    suggestions = []
-    if related_questions and related_searches:
-            suggestions += related_questions + related_searches
-    elif related_searches:
-        suggestions += related_searches
-    elif related_questions:
-        suggestions += related_questions
-
-    # Call OpenAI API in parallel for each suggestion
-    pool = ThreadPoolExecutor(max_workers=4)
-
-    # We don't want to call OpenAI more than 4 times, so limit the list length
-    if len(suggestions) <= 4:
-        # This returns a list in order after all tasks are complete
-        results = list(pool.map(prompt_response, suggestions))
-    else:
-        results = list(pool.map(prompt_response, suggestions[:4]))
-
-    # Match titles to thread results
-    res = []
-    suggestions_res = {"suggestions": res}
-
-    for i in range(len(results)):
-        res.append({
-            "title": suggestions[i],
-            "description": results[i]
-        })
-
-    # Jsonify and return as arrays of answers
-    return suggestions_res
 
 
 # To return a random tip during loading
